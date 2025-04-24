7c836b40983190f9ae5d9d2142ca1ca657a99a3b
manyajuneja1 <manyajuneja02@gmail.com>
2025-04-24 16:36:23 +0530

    Serve media files in production

--- patients/urls.py
+++ patients/urls.py
@@ -25,5 +25,7 @@ urlpatterns = [
 ]
 
 # Serve media/static files during development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
